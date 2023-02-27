74907c2a1733d10e18b29523092aa35ce1a2e661
tiago-bacelar <pereira.migueltiago@gmail.com>
2023-02-27 14:47:56 +0000

    Removed regex from tpc2

--- TPC2/tpc2.py
+++ TPC2/tpc2.py
@@ -1,18 +1,48 @@
 import sys
-import re
+           
+def suffixes(list):
+    return [list[i:] for i in range(0, len(list))]    
 
-pattern = re.compile(r'On|Off|\d+|=')
+def digits(input):
+    i = 0
+    while input[i] >= '0' and input[i] <= '9':
+        i += 1
+    return i
+
+def match_len(input, word):
+    input = input.lower()
+    word = word.lower()
+    
+    if word == '\\d+':
+        return digits(input)
+    elif input.startswith(word):
+        return len(word)
+    else:
+        return 0
+ 
+def get_matches(input, *words):
+    pos = 0
+    while pos < len(input):
+        suffix = input[pos:]
+        pos += 1
+        
+        for i, word in enumerate(words):
+            aux = match_len(suffix, word)
+            if aux != 0:
+                yield (i, suffix[:aux])
+                pos += aux - 1
+                break
 
 on = True
 sum = 0
 
 for line in sys.stdin:
-    for m in re.finditer(pattern, line):
-        if m.group() == 'On':
+    for i, m in get_matches(line, 'On', 'Off', '\\d+', '='):
+        if i == 0:
             on = True
-        elif m.group() == 'Off':
+        elif i == 1:
             on = False
-        elif m.group() == '=':
+        elif i == 2 and on:
+            sum += int(m)
+        elif i == 3:
             print(sum)
-        elif on:
-            sum += int(m.group())
